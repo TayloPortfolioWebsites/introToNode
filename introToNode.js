379b4417bebef6b34e8dcffb46781d9182af7c1d
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-04-23 22:17:22 +0800

    add supervillain list and names

--- introToNode.js
+++ introToNode.js
@@ -2,18 +2,29 @@
 const fs = require('fs');
 
 fs.copyFileSync('source.txt','destination.txt');
+
 */
+/************************************************** */
+
+// var superheroes = require('superheroes');
+
+// var superHeroNames = superheroes.random();
+
+// console.log(superHeroNames);
 
 /************************************************** */
 
-var superheroes = require('superheroes');
+// var superHeroList = superheroes.all;
 
-var superHeroNames = superheroes.random();
+// console.log(superHeroList);
 
-console.log(superHeroNames);
+/************************************************* */
 
-/************************************************** */
+var supervillains = require('supervillains');
 
-var superHeroList = superheroes.all;
+var superVillainNames = supervillains.random();
 
-console.log(superHeroList);
+console.log(superVillainNames);
+console.log(supervillains.all);
+
+/************************************************* */
