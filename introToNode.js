963eab284b0db022819c60713304d8512e983e23
imhazeltaylo <hazeltiffany.taylo.2342@gmail.com>
2023-04-23 22:07:41 +0800

    add superheroes list variable

--- introToNode.js
+++ introToNode.js
@@ -4,3 +4,16 @@ const fs = require('fs');
 fs.copyFileSync('source.txt','destination.txt');
 */
 
+/************************************************** */
+
+var superheroes = require('superheroes');
+
+var superHeroNames = superheroes.random();
+
+console.log(superHeroNames);
+
+/************************************************** */
+
+var superHeroList = superheroes.all;
+
+console.log(superHeroList);
